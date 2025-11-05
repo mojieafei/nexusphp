20e2e0f7030cd8182ae41e5983e9b197d8b0b8c1
mengting.yan <yanmengting_v@juzishuke.com>
2025-11-05 14:19:00 +0800

    修改种子列表的置顶底色

--- include/functions.php
+++ include/functions.php
@@ -5022,11 +5022,12 @@ function get_torrent_bg_color($promotion = 1, $posState = "", array $torrent = [
 	}
 	if (is_null($sphighlight)) {
         $torrentSettings = get_setting('torrent');
-	    if ($posState == \App\Models\Torrent::POS_STATE_STICKY_FIRST && !empty($torrentSettings['sticky_first_level_background_color'])) {
-	        $sphighlight = sprintf(' style="background-color: %s"', $torrentSettings['sticky_first_level_background_color']);
-        } elseif ($posState == \App\Models\Torrent::POS_STATE_STICKY_SECOND && !empty($torrentSettings['sticky_second_level_background_color'])) {
-            $sphighlight = sprintf(' style="background-color: %s"', $torrentSettings['sticky_second_level_background_color']);
-        }
+	    // 置顶背景色已禁用 - 使用CSS左侧金线标识置顶即可
+	    // if ($posState == \App\Models\Torrent::POS_STATE_STICKY_FIRST && !empty($torrentSettings['sticky_first_level_background_color'])) {
+	    //     $sphighlight = sprintf(' style="background-color: %s"', $torrentSettings['sticky_first_level_background_color']);
+        // } elseif ($posState == \App\Models\Torrent::POS_STATE_STICKY_SECOND && !empty($torrentSettings['sticky_second_level_background_color'])) {
+        //     $sphighlight = sprintf(' style="background-color: %s"', $torrentSettings['sticky_second_level_background_color']);
+        // }
     }
 	return apply_filter('torrent_background_color', (string)$sphighlight, $torrent);
 }
