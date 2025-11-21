4d65aacd583dc4a8fee3c5248b768374682eab6b
mengting.yan <yanmengting_v@juzishuke.com>
2025-11-21 15:01:04 +0800

    fix

--- include/cleanup_cli.php
+++ include/cleanup_cli.php
@@ -27,7 +27,8 @@ $begin = time();
 if ($force) {
     $result = docleanup(1, true);
 } else {
-    $result = autoclean(true);
+    // 直接执行 cleanup，不检查时间间隔（因为每分钟执行一次，时间间隔检查会导致并发问题）
+    $result = docleanup(0, true);
 }
 $log = "$logPrefix, DONE: $result, cost time in seconds: " . (time() - $begin);
 do_log($log);
