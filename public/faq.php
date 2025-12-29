8334e51aece988d6a4fa5804309778d454bcfa1a
mojieafei <daveafei@hotmail.com>
2025-12-29 11:42:21 +0800

    常见问题 显示 做种积分

--- public/faq.php
+++ public/faq.php
@@ -4,7 +4,55 @@ dbconn();
 require_once(get_langfile_path());
 //loggedinorreturn();
 
+/**
+ * 在等级提升说明中添加做种积分阈值信息
+ * @param string $answer FAQ答案的HTML内容
+ * @return string 添加了做种积分阈值后的HTML内容
+ */
+function add_seed_points_to_promotion_faq($answer) {
+	// 获取账号设置（和settings.php一样的方式）
+	$ACCOUNT = get_setting_from_db('account', []);
+	
+	// 等级映射：等级名称 => [等级常量, 默认值]
+	$classMapping = [
+		'PowerUser' => [UC_POWER_USER, \App\Models\User::$classes[UC_POWER_USER]['min_seed_points'] ?? 0],
+		'EliteUser' => [UC_ELITE_USER, \App\Models\User::$classes[UC_ELITE_USER]['min_seed_points'] ?? 0],
+		'CrazyUser' => [UC_CRAZY_USER, \App\Models\User::$classes[UC_CRAZY_USER]['min_seed_points'] ?? 0],
+		'InsaneUser' => [UC_INSANE_USER, \App\Models\User::$classes[UC_INSANE_USER]['min_seed_points'] ?? 0],
+		'VeteranUser' => [UC_VETERAN_USER, \App\Models\User::$classes[UC_VETERAN_USER]['min_seed_points'] ?? 0],
+		'ExtremeUser' => [UC_EXTREME_USER, \App\Models\User::$classes[UC_EXTREME_USER]['min_seed_points'] ?? 0],
+		'UltimateUser' => [UC_ULTIMATE_USER, \App\Models\User::$classes[UC_ULTIMATE_USER]['min_seed_points'] ?? 0],
+		'NexusMaster' => [UC_NEXUS_MASTER, \App\Models\User::$classes[UC_NEXUS_MASTER]['min_seed_points'] ?? 0],
+	];
+	
+	// 为每个等级添加做种积分阈值
+	foreach ($classMapping as $className => $classInfo) {
+		$class = $classInfo[0];
+		$defaultSeedPoints = $classInfo[1];
+		// 配置键名格式和settings.php一样：$class . "_min_seed_points"
+		$configKey = $class . '_min_seed_points';
+		// 获取做种积分阈值（和settings.php一样的逻辑）
+		$seedPoints = isset($ACCOUNT[$configKey]) ? (int)$ACCOUNT[$configKey] : $defaultSeedPoints;
+		
+		// 如果做种积分阈值大于0，则添加
+		if ($seedPoints > 0) {
+			// 在包含该等级名称的tr标签中，找到"分享率大于数字"的位置，在其前面添加"，做种积分大于X"
+			// 匹配模式：在包含该等级名称的tr标签中，找到"分享率大于数字"
+			$pattern = '/(<tr[^>]*>.*?<b[^>]*class="' . preg_quote($className, '/') . '_Name"[^>]*>.*?分享率大于[\d.]+)([。，<])/s';
+			$replacement = '$1，做种积分大于' . number_format($seedPoints) . '$2';
+			$answer = preg_replace($pattern, $replacement, $answer);
+		}
+	}
+	
+	return $answer;
+}
+
 stdhead($lang_faq['head_faq']);
+// 如果URL中有nocache参数，清除缓存
+if (isset($_GET['nocache'])) {
+	$Cache->delete_value('faq');
+	$Cache->delete_value('faq_page');
+}
 $Cache->new_page('faq', 900, true);
 if (!$Cache->get_page())
 {
@@ -88,7 +136,12 @@ if (isset($faq_categ)) {
 					if ($faq_categ[$id]['items'][$id2]['flag'] != "0")
 					{
 						print("<br /><span id=\"id".$faq_categ[$id]['items'][$id2]['link_id']."\"><b>". $faq_categ[$id]['items'][$id2]['question'] ."</b></span><br />\n");
-						print("<br />". $faq_categ[$id]['items'][$id2]['answer'] ."\n<br /><br />\n");
+						$answer = $faq_categ[$id]['items'][$id2]['answer'];
+						// 如果是等级提升说明的问题（link_id = 23），添加做种积分阈值信息
+						if ($faq_categ[$id]['items'][$id2]['link_id'] == 23) {
+							$answer = add_seed_points_to_promotion_faq($answer);
+						}
+						print("<br />". $answer ."\n<br /><br />\n");
 					}
 				}
 			}
